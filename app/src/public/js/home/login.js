f8c35ee59e2d5d5cd5ae2c9011c4be1d0fa18ad0
enots0514 <aattitude@daum.net>
2021-10-08 20:28:58 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,6 +12,13 @@ function login(){
        id : id.value,
        pwd : pwd.value
    };
+    //  console.log(req, JSON.stringify(req));
 
-   console.log(req);
+  fetch('/login', {
+      method: "POST",
+      header: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req)
+  });
 }
