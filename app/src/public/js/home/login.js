9a1d1a887e8ac39af17a29148afff42c3c68ef90
enots0514 <aattitude@daum.net>
2021-10-08 20:12:07 +0900

    don을 이용해서 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector('#id');
+const pwd = document.querySelector('#pwd');
+const loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener('click', login);
+
+function login(){
+    // console.log(id.value);
+   const req = {
+       id : id.value,
+       pwd : pwd.value
+   };
+
+   console.log(req);
+}
